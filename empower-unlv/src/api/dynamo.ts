0c196569abd4ec0052494592f33e8effa67d3d83
Taylor Finelli <taylorfinelli25@gmail.com>
2024-09-20 01:45:29 -0700

    ddb set up and can write to table

---
+++ empower-unlv/src/api/dynamo.ts
@@ -0,0 +1,31 @@
+import { DynamoDB } from "aws-sdk";
+
+const region = import.meta.env.VITE_AWS_REGION;
+const tableName = import.meta.env.VITE_DDB_TABLE_NAME;
+
+const awsAccessKeyId = import.meta.env.VITE_AWS_ACCESS_KEY;
+const awsSecretAccessKey = import.meta.env.VITE_AWS_SECRET_ACCESS_KEY;
+
+const dynamoDB = new DynamoDB.DocumentClient({
+  region: region,
+  credentials: {
+    accessKeyId: awsAccessKeyId,
+    secretAccessKey: awsSecretAccessKey,
+  },
+});
+
+export const writeData = async (item: any) => {
+  console.log(item);
+  const params = {
+    TableName: tableName,
+    Item: item,
+  };
+
+  try {
+    await dynamoDB.put(params).promise();
+    console.log("Data written successfully: ", item);
+  } catch (error) {
+    console.error("Error writing data: ", error);
+    throw error;
+  }
+};
--- empower-unlv/src/pages/data/index.tsx
+++ empower-unlv/src/pages/data/index.tsx
@@ -1,3 +1,32 @@
+import { writeData } from "@/api/dynamo";
+import { useState } from "react";
+
 export default function Data() {
-  return <div>Data</div>;
+  const [field1, setfield1] = useState("");
+  const [field2, setfield2] = useState("");
+  const [field3, setfield3] = useState("");
+  let item: any = { id: field1, graphId: field2, other: field3 };
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="id"
+        value={field1}
+        onChange={(e) => setfield1(e.target.value)}
+      ></input>
+      <input
+        type="text"
+        placeholder="graphId"
+        value={field2}
+        onChange={(e) => setfield2(e.target.value)}
+      ></input>
+      <input
+        type="text"
+        placeholder="other"
+        value={field3}
+        onChange={(e) => setfield3(e.target.value)}
+      ></input>
+      <button onClick={() => writeData(item)}>Write</button>
+    </div>
+  );
 }
