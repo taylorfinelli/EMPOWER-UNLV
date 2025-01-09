a2e4aec05a9b6d85859face4feb7f9686fe97fd6
Taylor Finelli <85365584+taylorfinelli@users.noreply.github.com>
2025-01-08 16:16:49 -0800

    Tracking ip locations (#12)
    * track country data in new DDB table
    
    * new API for geo data

--- empower-unlv/src/api/visitors.ts
+++ empower-unlv/src/api/visitors.ts
@@ -3,9 +3,12 @@ import iso from "iso-3166-1";
 
 const region = import.meta.env.VITE_AWS_REGION;
 const empowerVisitorsTableName = import.meta.env.VITE_DDB_VISITOR_TABLE_NAME;
+const empowerCountryCountsTableName = import.meta.env.VITE_DDB_COUNTRY_TABLE_NAME;
 
 const awsAccessKeyId = import.meta.env.VITE_AWS_ACCESS_KEY;
 const awsSecretAccessKey = import.meta.env.VITE_AWS_SECRET_ACCESS_KEY;
+
+const IPINFO_API_KEY = import.meta.env.VITE_IPINFO_API_KEY;
 
 const dynamoDB = new DynamoDB.DocumentClient({
   region: region,
@@ -20,7 +23,9 @@ export async function getData() {
   const response = await fetch("https://api.ipify.org?format=json");
   const ipAddress = (await response.json()).ip;
   // capture geolocation information
-  const ipInfo = await (await fetch(`https://ipapi.co/${ipAddress}/json/`)).json();
+  const ipInfo = await (
+    await fetch(`https://ipinfo.io/${ipAddress}/json?token=${IPINFO_API_KEY}`)
+  ).json();
   return ipInfo;
 }
 
@@ -33,6 +38,7 @@ export async function handleData(data: any) {
 
   try {
     const response = await dynamoDB.get(getParams).promise();
+    // if client IP does not exist in DB, log it
     if (!response.Item) {
       logData(data);
     }
@@ -57,9 +63,44 @@ export async function logData(data: any) {
   };
 
   try {
+    // log client data
     await dynamoDB.put(putParams).promise();
+    handleCountryData(info.country_code);
   } catch (error) {
-    console.error("Error fetching item: ", error);
+    console.error("Error writing item: ", error);
     throw error;
+  }
+}
+
+export async function handleCountryData(countryCode: string) {
+  const updateParams = {
+    TableName: empowerCountryCountsTableName,
+    Key: { countryCode },
+    UpdateExpression: "SET amount = if_not_exists(amount, :start) + :increment",
+    ExpressionAttributeValues: {
+      ":increment": 1,
+      ":start": 0,
+    },
+    ReturnValues: "ALL_NEW",
+  };
+
+  try {
+    // DDB's update() will insert the item if it doesn't exist
+    await dynamoDB.update(updateParams).promise();
+  } catch (error: any) {
+    if (error.code === "ConditionalCheckFailedException") {
+      // in case of a conditional failure, meaning item doesn't exist, perform a put
+      const putParamsDNE = {
+        TableName: empowerCountryCountsTableName,
+        Item: {
+          countryCode: countryCode,
+          amount: 1,
+        },
+      };
+      await dynamoDB.put(putParamsDNE).promise();
+    } else {
+      console.error("Error handling country data:", error);
+      throw error;
+    }
   }
 }
